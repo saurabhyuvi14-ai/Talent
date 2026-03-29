ff827eaa4cf33412ca725a389e193abb411b82fc
Saurabh kumar <saurabhkumar@Saurabhs-MacBook-Pro.local>
2026-03-29 14:43:20 +0530

    changes made

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        results: resolve(__dirname, 'results.html'),
+        fees: resolve(__dirname, 'fees.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+})
